3868163d349284e8b85343f1a5e1a234218aa51a
Samik Pradhan <samikpradhan@gmail.com>
2023-07-30 11:15:57 -0700

    cleaning up Platform.h

--- include/Platform.h
+++ include/Platform.h
@@ -5,16 +5,12 @@
 class Platform
 {
 private:
-    // Vector2 size;
-    // Vector2 position;
     Rectangle rect;
     Color tile;
-    // Texture2D tile;
 
 public:
     Platform(Vector2 position, Vector2 size, Color tile);
     void draw_tile();
     Vector2 get_size();
     Vector2 get_position();
-    // Color get_tile();
 };
