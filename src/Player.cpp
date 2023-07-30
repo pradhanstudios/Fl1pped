f3a4800ed5ea997b7bf8b123a1e1a390ea3fe87f
Samik Pradhan <samikpradhan@gmail.com>
2023-07-29 19:10:23 -0700

    defined some Player variables and updated header file

---
+++ src/Player.cpp
@@ -0,0 +1,12 @@
+#include "raylib.h"
+#include "Player.h"
+
+class Player
+{
+private:
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 acceleration;
+    Vector2 size;
+    Texture2D skin;
+};
