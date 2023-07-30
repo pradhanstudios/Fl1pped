b9157f4467f8592f8d75312e68e3bb6d79bb0807
Ritik <flipit001@gmail.com>
2023-07-29 19:36:26 -0700

    added basic ground

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ int main(void)
     const int screen_width = 1200;
     const int screen_height = 800;
 
+    // ground
+    Vector2 ground_size = {screen_width / 2, screen_height / 12};
+    Vector2 ground_pos = {ground_size.x / 2, (float)(screen_height - ground_size.y)};
+
     // initialize window
     InitWindow(screen_width, screen_height, "Window Title");
 
@@ -23,6 +27,9 @@ int main(void)
         // DRAW
         // ---------------------------------------------------------
         BeginDrawing();
+
+        // draw ground
+        DrawRectangleV(ground_pos, ground_size, GREEN);
 
         // refresh background
         ClearBackground(RAYWHITE);
