18d974482345ee93b98bb5b33197169b8512052e
Samik Pradhan <samikpradhan@gmail.com>
2023-07-29 19:24:12 -0700

    updating Player

--- include/Player.h
+++ include/Player.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "raylib.h"
+
+#define FRICTION (double)-0.12;
+
+class Player
+{
+private:
+    Vector2 size;
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 acceleration;
+    // Texture2D skin;
+
+public:
+    Player(Vector2 position, Vector2 velocity, Vector2 acceleration, Vector2 size); // TODO: implement Texture2D skin
+    void move();
+    void jump();
+};
