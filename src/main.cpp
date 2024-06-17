c95e298d2d7fed5db0a9bde69ddf88b4ccdbcea9
ritik <flipit001@gmail.com>
2024-06-17 10:34:08 -0700

    added template code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,29 @@
 #include "raylib.h"
 
 int main() {
-    
+
+    const int screen_width = 1200;
+    const int screen_height = 800;
+
+    // init window
+    InitWindow(screen_width, screen_height, "Title");
+
+    // set fps
+    SetTargetFPS(60);
+
+    // game loop
+    while (!WindowShouldClose()) {
+        // udpate
+
+        // draw
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
